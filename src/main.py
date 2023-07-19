84fa1f159a13fb5fd000ba2c52d2e86947e5dce2
Aymen Ben Ali <ibnali.aymen@gmail.com>
2023-07-19 16:07:49 +0200

    refactor code

--- src/main.py
+++ src/main.py
@@ -1,54 +1,32 @@
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import sum, col, desc, regexp_extract, udf, mean, lit, to_date, substring, asc
-from pyspark.sql.types import StringType
+import queries as q
+
+def main():
+    #load csv file into dataframe
+    walmart_store_sales = q.read_csv("./data/Walmart_Store_sales.csv")
+    store_address = q.read_csv("./data/store_address.csv")
+
+    #join store_address to get store address
+    output_df = q.add_address(walmart_store_sales,store_address)
+    output_df.show()
+
+    #find the store with max sales
+    store_with_max_sales = q.calculate_store_with_max_sales(output_df)
+    print(f"The store with max sales is {store_with_max_sales}")
+
+    #extract zip_code from address
+    output_df_with_zip_code = q.extract_zip_code(output_df)
+    output_df.show()
+
+    #if the total sales is higher than the mean of  all sales then classification= "High sales"
+    #  otherwise classification = "Low Sales"
+    average_sales = q.calculate_average_sales(output_df_with_zip_code)
+    output_df_classified = q.classify_sales(output_df_with_zip_code, average_sales)
+    output_df_classified.show()
+
+    #get the month that has the minimum sales
+    month_with_min_sales = q.calculate_month_with_min_sales(output_df_with_zip_code)
+    print(f"the month that has the minimum sales is {month_with_min_sales}")
 
 
-spark = SparkSession.builder.master("local[1]").appName("workshop-Walmart").getOrCreate()
-
-def read_csv(path: str)-> DataFrame:
-    return spark.read.option("header",True).option("inferSchema",True) \
-        .option("delimiter",",") \
-        .csv(path)
-
-walmart_store_sales = read_csv("./data/Walmart_Store_sales.csv")
-store_address = read_csv("./data/store_address.csv")
-
-output_df = walmart_store_sales.join(store_address,"Store", "inner")
-
-
-store_with_max_sales = output_df.groupBy("Store") \
-                                .agg(sum("Weekly_Sales").alias("sum_sales")) \
-                                .orderBy(col("sum_sales").desc()) \
-                                .first()["Store"]
-
-print(store_with_max_sales)
-
-store_address.withColumn("zip_code",regexp_extract(store_address.Address, r'(\d+)$', 1)) \
-    .show()
-
-
-#if the total sales is higher than the mean of  all sales then classification= "High sales"
-#  otherwise classification = "Low Sales"
-
-@udf(returnType=StringType()) 
-def classificationUDF(store_sales,mean):
-    if store_sales > mean:
-        return "High sales"
-    else:
-        return "Low sales"
-
-average_sales = walmart_store_sales.select(mean(col("Weekly_Sales")).alias("average_sales")) \
-    .first()["average_sales"]
-
-walmart_store_sales.select(col("Store"), classificationUDF(col("Weekly_Sales"), lit(average_sales)).alias("classification")) \
-                   .show()
-                      
-#get the month that has the minimum sales
-
-month_with_min_sales = walmart_store_sales.withColumn("month", substring("Date", 4,8)) \
-                                .groupBy("month") \
-                                .agg(sum("Weekly_Sales").alias("sum_sales")) \
-                                .orderBy(col("sum_sales").asc()) \
-                                .first()["month"]
-
-print(month_with_min_sales)
+if __name__ == "__main__":
+    main()
